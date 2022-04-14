4b2ef831fcb4f2fa60825823b605998b97dcba4f
Jamci <wet2005_001@163.com>
2022-04-14 09:12:49 +0800

    commit feature 1.feature add start parameters -p is set port func

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"log"
@@ -14,15 +16,43 @@ var upGrader = websocket.Upgrader{
 	},
 }
 
+// 定义端口号参数形式
+var port = flag.Int("p", 8000, "请使用int类型参数")
+
 func main() {
-	start()
+	flag.Parse()
+	fmt.Println("-port:", *port)
+	start(*port)
 }
 
-func start() {
+func start(port int) {
+	address := fmt.Sprintf("%s:%d", "localhost", port)
 	router := gin.New()
-	log.Println("Serving at localhost:8000...")
-	if err := router.Run(":8000"); err != nil {
+	router.GET("/", getMessage)
+	router.Run(fmt.Sprintf(":%d", port))
+	log.Println("Serving at " + address)
+	if err := router.Run(fmt.Sprintf(":%d", port)); err != nil {
 		log.Fatal("failed run app: ", err)
+
+	}
+}
+
+func getMessage(c *gin.Context) {
+	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	defer ws.Close()
+	for {
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Println(string(message))
+		err = ws.WriteMessage(mt, message)
+		if err != nil {
+			break
+		}
 
 	}
 }
